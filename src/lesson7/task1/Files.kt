e47a6fcdf39b84674a9c468cbb0ff5aabc473021
reshe <reshetihink@mail.ru>
2022-12-10 17:26:04 +0300

    тест задания printDivisionProcess

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -493,7 +493,6 @@ fun printDivisionProcess(lhv: Int, rhv: Int, outputName: String) {
                     .println("-$currentDivisor${" ".repeat(sizeForSpace - "-$currentDivisor".length)}${lhv / rhv}")
                 writer.println("-".repeat("-$currentDivisor".length))
                 sizeForSpace = "-$currentDivisor".length - "$currentMod".length
-                currentDivisible = currentMod
                 firstDivisible = false
             } else {
                 sizeForSpace -= if (zeroCurrentMod) {
@@ -510,7 +509,10 @@ fun printDivisionProcess(lhv: Int, rhv: Int, outputName: String) {
             if (currentMod == 0) {
                 currentDivisible = 0
                 zeroCurrentMod = true
-            } else zeroCurrentMod = false
+            } else {
+                currentDivisible = currentMod
+                zeroCurrentMod = false
+            }
             if (index == listOfLHVDigits.lastIndex) {
                 writer.print("${" ".repeat(sizeForSpace)}${lhv % rhv}")
             }
